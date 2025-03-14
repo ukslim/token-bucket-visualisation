7d4c7a53da850e8483b051a8a6ada5749b658780
John Hartnup <john.hartnup@wealthwizards.com>
2025-03-14 16:50:18 +0000

    try different vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from "vite";
 
-// Get the repository name from package.json or environment
-const repoName = "token-bucket-visualisation";
-
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? `/${repoName}/` : "/",
+  base: "./",
 });
